0ad67d13bdb7a3e65c97ebd016cdee7afc33483f
NoaelSameer <nsamee443@west-mec.org>
2023-09-22 18:22:43 -0700

    added decimal to hex converter

--- Project.py
+++ Project.py
@@ -2,6 +2,12 @@ decimal = float(input("Enter a decimal number:  "))
 if decimal.is_integer():
     decimal= int(decimal)
     binary= bin(decimal)
+
     print("The binary number for " + str(decimal) + " is: " + str(binary))
 else:
     print("Enter a valid number")
+
+decimal = hex(int(input("Enter a decimal number:  ")))
+decimal = decimal.replace("0x", "")
+print(decimal)
+
